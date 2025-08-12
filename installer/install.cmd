7b879c7c998c06dea0950b6f5a2014b9cdd5baa5
eiedouno <mc_master47@outlook.com>
2025-08-12 18:50:13 -0500

    update 01.0

--- installer/install.cmd
+++ installer/install.cmd
@@ -1,7 +1,7 @@
 @echo off
 powershell -Command "New-Item -Path 'PowerServer-Installer' -ItemType Directory -Force"
 powershell -Command "New-Item -Path 'PowerServer-Installer\bin' -ItemType Directory -Force"
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eiedouno/PowerServer/stable/installer/LICENSE.txt' -OutFile 'PowerServer-Installer\LICENSE.txt'"
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eiedouno/PowerServer/stable/installer/bin/install.ps1' -OutFile 'PowerServer-Installer\bin\install.ps1'"
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eiedouno/PowerServer/stable/installer/bin/initiate.ps1' -OutFile 'PowerServer-Installer\bin\initiate.ps1'"
-powershell -File "ToolBored-Installer\bin\install.ps1"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eiedouno/PowerServer/main/installer/LICENSE.txt' -OutFile 'PowerServer-Installer\LICENSE.txt'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eiedouno/PowerServer/main/installer/bin/install.ps1' -OutFile 'PowerServer-Installer\bin\install.ps1'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/eiedouno/PowerServer/main/installer/bin/initiate.ps1' -OutFile 'PowerServer-Installer\bin\initiate.ps1'"
+powershell -File "PowerServer-Installer\bin\install.ps1"

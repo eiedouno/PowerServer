0d1f9ffe8f9ecca52e05c00665811f85f82a7f81
eiedouno <mc_master47@outlook.com>
2025-08-12 20:19:48 -0500

    updated update switch

--- pwserver.ps1
+++ pwserver.ps1
@@ -22,7 +22,7 @@ Start-Sleep -Seconds 1
 if ($Update) {
     try {
         Write-Host "Updating PowerServer..." -ForegroundColor Blue
-        & "$PSScriptRoot\bin\update.ps1"
+        & "$PSScriptRoot\bin\update.ps1" -Wait
         Write-Host "`nUpdated PowerServer.`n" -ForegroundColor Green
         exit
     } catch {

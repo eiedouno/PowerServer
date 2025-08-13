7e4914f644d275f11cf80f77e27dd48ffa8fa9e5
eiedouno <mc_master47@outlook.com>
2025-08-12 19:34:29 -0500

    updated update switch

--- bin/update.ps1
+++ bin/update.ps1
@@ -1,9 +1,9 @@
-$buildInfo = Get-Content -Path "$PSScriptRoot\bin\buildinfo.json" -Raw | ConvertFrom-Json
+$buildInfo = Get-Content -Path "$PSScriptRoot\buildinfo.json" -Raw | ConvertFrom-Json
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/eiedouno/PowerServer/main/installer/bin/initiate.ps1" -OutFile .\bin\initiate-update.ps1
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/eiedouno/PowerServer/main/installer/bin/initiate.ps1" -OutFile "$PSScriptRoot\initiate-update.ps1"
 
 Start-Process -FilePath "powershell.exe" -ArgumentList @(
-    "-File", "$PSScriptRoot\bin\initiate-update.ps1",
+    "-File", "$PSScriptRoot\initiate-update.ps1",
     "-Path $($buildInfo.Path)",
     "-Version $($buildInfo.Version)",
     "-License", "true",

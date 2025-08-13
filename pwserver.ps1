791360916966592748d60476c7379c89d623723c
eiedouno <mc_master47@outlook.com>
2025-08-12 19:31:37 -0500

    updated update switch

--- pwserver.ps1
+++ pwserver.ps1
@@ -17,7 +17,7 @@ param (
 )
 
 if ($Update) {
-    $PSSCriptRoot\bin\update.ps1
+    powershell $PSSCriptRoot\bin\update.ps1
 }
 
 if (($Path -eq "HELPMSG") -or ($Path -eq "/?") -or ($Help) -and -not $SetDefault -and -not ($Default)) {
